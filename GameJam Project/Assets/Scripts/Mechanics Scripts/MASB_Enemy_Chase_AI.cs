a24bd9f7c3d5030ee42010115fc32ada372c1994
MartinJimbobBrockett <57472839+JimbobBrockett@users.noreply.github.com>
2019-11-23 17:24:22 +0000

    Reset scene script added, basic AI added,

---
+++ GameJam Project/Assets/Scripts/Mechanics Scripts/MASB_Enemy_Chase_AI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MASB_Enemy_Chase_AI : MonoBehaviour
+{
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GetComponent<MeshRenderer>().isVisible)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, 5 * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.GetComponent<MASB_Player_Health_Controller>().damagePlayer(2);
+        }
+    }
+}
---
+++ GameJam Project/Assets/Scripts/Mechanics Scripts/MASB_Player_Health_Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MASB_Player_Health_Controller : MonoBehaviour
+{
+    public int playerHealth = 10;
+    private GameObject manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.Find("GameManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHealth <= 0)
+        {
+            killPlayer();
+        }
+    }
+
+    public void damagePlayer(int damage)
+    {
+        playerHealth -= damage;
+        Debug.Log(playerHealth);
+    }
+
+    void killPlayer()
+    {
+        manager.GetComponent<MASB_Reset_Scene>().resetScene();
+    }
+}
