8bf2d4da7ba44b2bf450508cad3f46ffbb88176f
MartinJimbobBrockett <57472839+JimbobBrockett@users.noreply.github.com>
2019-11-23 08:30:06 +0000

    Cube rotation mostly done

--- GameJam Project/Assets/Scripts/Mechanical Scripts/MASB_Rotate_Map.cs
+++ GameJam Project/Assets/Scripts/Mechanical Scripts/MASB_Rotate_Map.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MASB_Rotate_Map : MonoBehaviour
 {
     public GameObject player;
+    public GameObject manager;
     public float scale;
     public bool isPlayerFrozen = false;
 
@@ -12,6 +13,9 @@ public class MASB_Rotate_Map : MonoBehaviour
     void Start()
     {
         scale = GameObject.Find("Cube").transform.localScale.x;
+        player = GameObject.Find("Player");
+        manager = GameObject.Find("CubeManager");
+        transform.eulerAngles = new Vector3(manager.GetComponent<MASB_Cube_Manager>().curRotX, 0, manager.GetComponent<MASB_Cube_Manager>().curRotZ);
     }
 
     // Update is called once per frame
@@ -37,49 +41,151 @@ public class MASB_Rotate_Map : MonoBehaviour
 
     void rotateMap(string direction)
     {
-        if (!isPlayerFrozen)
+        if (player.GetComponent<JDH_ThirdPersonController_Script>().playerData.IsPlayerMovable)
         {
             freezePlayer();
+            isPlayerFrozen = true;
         }
         if (direction == "x+")
         {
-            if (transform.eulerAngles.z < 90)
+            transform.RotateAround(Vector3.zero, Vector3.forward, 90 * Time.deltaTime);
+            if (transform.eulerAngles.z >= 89 && transform.eulerAngles.z < 90)
             {
-                transform.Rotate(0, 0, 90 * Time.deltaTime);
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotZ = 90;
+                player.transform.position = new Vector3(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
+                Destroy(gameObject);
+            } 
+            else if (transform.eulerAngles.z >= 179 && transform.eulerAngles.z < 180)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotZ = 180;
+                player.transform.position = new Vector3(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.z >= 269 && transform.eulerAngles.z < 270)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotZ = 270;
+                player.transform.position = new Vector3(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.z >= 359)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotZ = 0;
+                player.transform.position = new Vector3(player.transform.position.x - 1, player.transform.position.y, player.transform.position.z);
+                Destroy(gameObject);
             }
         }
         else if (direction == "x-")
         {
-            if (transform.eulerAngles.z > 270 || transform.eulerAngles.z == 0)
+            transform.RotateAround(Vector3.zero, Vector3.back, 90 * Time.deltaTime);
+            if (transform.eulerAngles.z <= 271 && transform.eulerAngles.z > 270)
             {
-                transform.Rotate(0, 0, -90 * Time.deltaTime);
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotZ = 270;
+                player.transform.position = new Vector3(player.transform.position.x + 1, player.transform.position.y, player.transform.position.z);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.z <= 181 && transform.eulerAngles.z > 180)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotZ = 180;
+                player.transform.position = new Vector3(player.transform.position.x + 1, player.transform.position.y, player.transform.position.z);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.z <= 91 && transform.eulerAngles.z > 90)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotZ = 90;
+                player.transform.position = new Vector3(player.transform.position.x + 1, player.transform.position.y, player.transform.position.z);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.z <= 1)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotZ = 0;
+                player.transform.position = new Vector3(player.transform.position.x + 1, player.transform.position.y, player.transform.position.z);
+                Destroy(gameObject);
             }
         }
         else if (direction == "z+")
         {
-            if (transform.eulerAngles.x < 90)
+            transform.RotateAround(Vector3.zero, Vector3.right, 90 * Time.deltaTime);
+            if (transform.eulerAngles.x >= 89 && transform.eulerAngles.x < 90)
             {
-                transform.Rotate(90 * Time.deltaTime, 0, 0);
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotX = 90;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 1);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.x >= 179 && transform.eulerAngles.x < 180)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotX = 180;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 1);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.x >= 269 && transform.eulerAngles.x < 270)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotX = 270;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 1);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.x >= 359)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotX = 0;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + 1);
+                Destroy(gameObject);
             }
         }
         else
         {
-            if (transform.eulerAngles.x > 270 || transform.eulerAngles.x == 0)
+            transform.RotateAround(Vector3.zero, Vector3.left, 90 * Time.deltaTime);
+            if (transform.eulerAngles.x <= 271 && transform.eulerAngles.x > 270)
             {
-                transform.Rotate(-90 * Time.deltaTime, 0, 0);
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotX = 270;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.x <= 181 && transform.eulerAngles.x > 180)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotX = 180;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.x <= 91 && transform.eulerAngles.x > 90)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotX = 90;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1);
+                Destroy(gameObject);
+            }
+            else if (transform.eulerAngles.x <= 1)
+            {
+                unfreezePlayer(false, direction);
+                manager.GetComponent<MASB_Cube_Manager>().curRotX = 0;
+                player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 1);
+                Destroy(gameObject);
             }
         }
     }
 
     void freezePlayer()
     {
-        player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
-        player.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-        isPlayerFrozen = true;
+        player.GetComponent<JDH_ThirdPersonController_Script>().playerData.IsPlayerMovable = false;
     }
 
-    void unfreezePlayer()
+    void unfreezePlayer(bool playerIsFrozen, string direction)
     {
-
+        if (!playerIsFrozen)
+        {
+            player.GetComponent<JDH_ThirdPersonController_Script>().playerData.IsPlayerMovable = true;
+        }
     }
 }
